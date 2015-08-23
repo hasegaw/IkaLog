bb0118dade79b54b6836e35d85b2853697cea82c
Takeshi HASEGAWA <hasegaw@gmail.com>
2015-08-23 11:34:18 +0900

    IkaScene_Screenshot: Capture Miiverse drawings with S key
    Signed-off-by: Takeshi HASEGAWA <hasegaw@gmail.com>

--- IkaOutput_Screenshot.py
+++ IkaOutput_Screenshot.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+
 from IkaUtils import *
+from IkaScene_PlazaUserStat import * #Fixme...
 
 import cv2
 import time
@@ -9,6 +11,19 @@ import time
 #
 # Save screenshots on certain events
 class IkaOutput_Screenshot:
+	def saveDrawing(self, context):
+		x1 = 241
+		x2 = x1 + 367
+		y1 = 528
+		y2 = y1 + 142
+
+		drawing = context['engine']['frame'][y1:y2, x1:x2, :]
+
+		basename = time.strftime("%Y%m%d_%H%M%S", time.localtime())
+		destfile = "%s/miiverse_%s.png" % (self.dest_dir, basename)
+
+		IkaUtils.writeScreenshot(destfile, drawing)
+		print("スクリーンショット %s を保存しました" % destfile)
 
 	##
 	# onGameIndividualResult Hook
@@ -21,6 +36,13 @@ class IkaOutput_Screenshot:
 
 		IkaUtils.writeScreenshot(destfile, context['engine']['frame'])
 		print("スクリーンショット %s を保存しました" % destfile)
+
+	def onKeyPress(self, context, key):
+		if not (key == 0x53 or key == 0x73):
+			return False
+
+		if IkaScene_PlazaUserStat().match(context):
+			self.saveDrawing(context)
 
 	##
 	# Constructor
