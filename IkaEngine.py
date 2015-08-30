7e6540bf766bf4c93a72d1a277a408643b767540
Takeshi HASEGAWA <hasegaw@gmail.com>
2015-08-30 20:58:08 +0900

    IkaEngine: Splited the core engine fro IkaLog.py.
    Signed-off-by: Takeshi HASEGAWA <hasegaw@gmail.com>

---
+++ IkaEngine.py
@@ -0,0 +1,200 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+#  IkaLog
+#  ======
+#  Copyright (C) 2015 Takeshi HASEGAWA
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#
+
+import sys
+import numpy as np
+import cv2
+import time
+import traceback
+
+from IkaScene_GameStart import *
+from IkaScene_ResultDetail import *
+from IkaScene_TowerTracker import *
+from IkaScene_InGame import *
+
+## The IkaLog core engine.
+#
+class IkaEngine:
+	scn_gamestart = IkaScene_GameStart()
+	scn_gameresult = IkaScene_ResultDetail()
+	scn_ingame = IkaScene_InGame()
+	scn_towerTracker = IkaScene_TowerTracker()
+	last_capture = time.time() - 100;
+	last_gamestart = time.time() - 100;
+
+	def dprint(self, text):
+		print(text, file = sys.stderr)
+
+	def callPlugins(self, event_name, debug = False):
+		if debug:
+			self.dprint("call plug-in hook (%s):" % event_name)
+		for op in self.OutputPlugins:
+			if hasattr(op, event_name):
+				if debug:
+					self.dprint("Call  %s" % op.__class__.__name__)
+				try:
+					getattr(op, event_name)(self.context)
+				except:
+					self.dprint("%s.%s() raised a exception >>>>" % (op.__class__.__name__, event_name))
+					self.dprint(traceback.format_exc())
+					self.dprint("<<<<<")
+
+	def reset(self):
+		# Initalize the context
+		self.context = {
+			"game": {
+				'map': None,
+				'rule': None,
+				'won': None,
+				'players': None,
+				'livesTrack': [],
+				'towerTrack': [],
+			},
+			"engine": {
+				'frame': None,
+			}
+		}
+
+	def processFrame(self):
+		context = self.context # Python のオブジェクトって参照だよね?
+		frame = self.readNextFrame(skip_frames = 12)
+		# FixMe: frame can be a null
+
+		context['engine']['frame'] = frame
+		context['engine']['inGame'] = self.scn_ingame.matchTimerIcon(context)
+
+		self.callPlugins('onFrameRead')
+
+		if context['engine']['inGame']:
+			tower_data = self.scn_towerTracker.match(context)
+
+			try:
+				# ライフをチェック
+				(team1, team2) = self.scn_ingame.lives(context)
+				# print("味方 %s 敵 %s" % (team1, team2))
+
+				context['game']['livesTrack'].append([team1, team2])
+				if tower_data:
+					context['game']['towerTrack'].append(tower_data.copy())
+			except:
+				pass
+
+		# GameStart (マップ名、ルール名が表示されている) ?
+
+		r = None
+		if (not context['engine']['inGame']) and (time.time() - self.last_gamestart) > 10:
+			r = self.scn_gamestart.match(context)
+
+		if r:
+			context["game"] =  {
+				'map': None,
+				'rule': None,
+				'livesTrack': [],
+				'towerTrack': [],
+			}
+			self.scn_towerTracker.reset(context)
+
+			while (r):
+				frame = self.capture.read()
+				frame = self.capture.read()
+				frame = self.capture.read()
+				context['engine']['frame'] = frame
+				r = self.scn_gamestart.match(context)
+
+			self.last_gamestart = time.time()	
+
+			self.callPlugins('onGameStart')
+		
+		# GameResult (勝敗の詳細が表示されている）?
+		r = (not context['engine']['inGame']) and (time.time() - self.last_capture) > 60
+		if r:
+			r = self.scn_gameresult.match(context)
+
+		if r:
+			if ((time.time() - self.last_capture) > 60):
+				self.last_capture = time.time()	
+				
+				# 安定するまで待つ
+				for x in range(10):
+					new_frame = self.capture.read()
+					if not (new_frame is None):
+						frame = new_frame
+
+				# 安定した画像で再度解析
+				context['engine']['frame'] = frame
+				self.scn_gameresult.analyze(context)
+
+				print("Finish検出")
+				self.callPlugins('onGameIndividualResultAnalyze')
+				self.callPlugins('onGameIndividualResult')
+				self.callPlugins('onGameReset')
+
+				self.reset()
+
+		key = None
+
+		# FixMe: Since onFrameNext and onKeyPress has non-standard arguments,
+		# self.callPlugins() doesn't work for those.
+
+		for op in self.OutputPlugins:
+			if hasattr(op, "onFrameNext"):
+				try:
+					key = op.onFrameNext(context)
+				except:
+					pass
+
+		for op in self.OutputPlugins:
+			if hasattr(op, "onKeyPress"):
+				try:
+					op.onKeyPress(context, key)
+				except:
+					pass
+
+	def readNextFrame(self, skip_frames = 0):
+		for i in range(skip_frames):
+			frame = self.capture.read()
+
+		while frame is None:
+			cv2.waitKey(1000)
+			frame = self.capture.read()
+		return frame
+
+	def run(self):
+		while True:
+			self.processFrame()
+
+		cv2.destroyAllWindows()
+
+	def setCapture(self, capture):
+		self.capture = capture
+
+	def setPlugins(self, plugins):
+		self.OutputPlugins = plugins
+
+	def __init__(self):
+		self.reset()
+
+if __name__ == "__main__":
+	from IkaConfig import *
+	_capture, _OutputPlugins = IkaConfig().config()
+	engine = IkaEngine()
+	engine.setCapture(_capture)
+	engine.setPlugins(_OutputPlugins)
+	engine.run()
