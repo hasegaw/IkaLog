hasegaw/IkaLog

commit e423104f8a80dba76affbe873324d147a7642362

IkaScene_(InGame|Lobby): Added events
IkaScene_InGame: Added events (onGoSign, onGameDead)
IkaScene_Lobby: Added

Signed-off-by: Takeshi HASEGAWA <hasegaw@gmail.com>

IkaEngine.py
@@ -28,6 +28,7 @@ from IkaScene_GameStart import *
 from IkaScene_ResultDetail import *
 from IkaScene_TowerTracker import *
 from IkaScene_InGame import *
+from IkaScene_Lobby import *
 
 ## The IkaLog core engine.
 #
@@ -36,8 +37,14 @@ class IkaEngine:
 	scn_gameresult = IkaScene_ResultDetail()
 	scn_ingame = IkaScene_InGame()
 	scn_towerTracker = IkaScene_TowerTracker()
-	last_capture = time.time() - 100;
-	last_gamestart = time.time() - 100;
+	scn_lobby = IkaScene_Lobby()
+
+	last_capture = time.time() - 100
+	last_gamestart = time.time() - 100
+	last_lobby_matching = time.time() - 100
+	last_lobby_matched = time.time() - 100
+	last_go_sign = time.time() - 100
+	last_dead = time.time() - 100
 
 	def dprint(self, text):
 		print(text, file = sys.stderr)
@@ -83,6 +90,21 @@ class IkaEngine:
 		self.callPlugins('onFrameRead')
 
 		if context['engine']['inGame']:
+			# ゴーサイン
+			r = False
+			if self.last_go_sign + 60 < time.time():
+				r = self.scn_ingame.matchGoSign(context)
+
+			if r:
+				self.last_go_sign = time.time()
+				self.callPlugins('onGoSign')
+
+			# 死亡状態（「復活まであとｎ秒」）
+			if self.scn_ingame.matchDead(context):
+				if last_dead + 3 < time.time():
+					self.callPlugins('onGameDead')
+				last_dead = time.time()
+
 			tower_data = self.scn_towerTracker.match(context)
 
 			try:
@@ -95,6 +117,24 @@ class IkaEngine:
 					context['game']['towerTrack'].append(tower_data.copy())
 			except:
 				pass
+
+		# Lobby
+		r = False
+		if not context['engine']['inGame']:
+			r = self.scn_lobby.match(context)
+
+		if r:
+			if context['game']['lobby']['state'] == 'matching':
+				if (time.time() - self.last_lobby_matching) > 60:
+					# マッチングを開始した
+					self.callPlugins('onLobbyMatching')
+				self.last_lobby_matching = time.time()
+
+			if context['game']['lobby']['state'] == 'matched':
+				if (time.time() - self.last_lobby_matched) > 10:
+					# マッチングした直後
+					self.callPlugins('onLobbyMatched')
+				self.last_lobby_matched = time.time()
 
 		# GameStart (マップ名、ルール名が表示されている) ?
 

IkaScene_InGame.py
@@ -157,13 +157,31 @@ class IkaScene_InGame:
 
 		return IkaUtils.matchWithMask(img, self.mask_timer, 0.85, 0.6)
 
+	def matchGoSign(self, context):
+		return self.mask_goSign.match(context['engine']['frame'])
+
+	def matchDead(self, context):
+		return self.mask_dead.match(context['engine']['frame'])
+
 	def __init__(self):
 		self.mask_timer = IkaUtils.loadMask('masks/ingame_timer.png', self.timer_left, self.timer_top, self.timer_top, self.timer_height)
+		self.mask_goSign = IkaMatcher(
+			1280 / 2 - 420 / 2, 130, 420, 170,
+			img_file = 'masks/ui_go.png',
+			threshold = 0.99,
+			orig_threshold = 0.5,
+			pre_threshold_value = 240,
+			label = 'Go!',
+		)
 
-		#img = cv2.imread('masks/ingame_timer.png')
-		#if img is None:
-		#	print("マスクデータ masks/ingame_timer.png が読み込めませんでした")
-		#self.mask_timer = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)[self.timer_top:self.timer_bottom, self.timer_left:self.timer_right]
+		self.mask_dead = IkaMatcher(
+			1057, 648, 140, 40,
+			img_file = 'masks/ui_dead.png',
+			threshold = 0.8,
+			orig_threshold = 0.3,
+			pre_threshold_value = 220,
+			label = 'dead',
+		)
 
 
 if __name__ == "__main__":

IkaScene_Lobby.py
@@ -1,0 +1,105 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+#  IkaLog
+#  ======
+#  Copyright (C) 2015 Takeshi HASEGAWA
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#
+import numpy as np
+import cv2
+import sys
+from IkaUtils import *
+
+class IkaScene_Lobby:
+	def match(self, context):
+		frame = context['engine']['frame']
+
+		# 「ルール」「ステージ」の文字があればパブリックロビーで待ち合わせ中
+		if not self.mask_rule.match(frame):
+			return False
+
+		if not self.mask_stage.match(frame):
+			return False
+
+		# マッチング状況はどちらかがあるはず
+		r_matching = self.mask_matching.match(frame)
+		r_matched  = self.mask_matched.match(frame)
+
+		context['game']['lobby'] = {}
+		context['game']['lobby']['public'] = True
+		context['game']['lobby']['state'] = None
+
+		if not (r_matching or r_matched):
+			return False
+
+		if (r_matching):
+			context['game']['lobby']['state'] = 'matching'
+		if (r_matched):
+			context['game']['lobby']['state'] = 'matched'
+
+		return True
+
+	def __init__(self):
+		self.mask_rule = IkaMatcher(
+			0, 220, 737, 94,
+			img_file = 'masks/ui_lobby_public.png',
+			threshold = 0.999,
+			orig_threshold = 0.5,
+			pre_threshold_value = 220,
+			label = 'Rule'
+		)
+
+		self.mask_stage = IkaMatcher(
+			0, 345, 737, 94,
+			img_file = 'masks/ui_lobby_public.png',
+			threshold = 0.999,
+			orig_threshold = 0.5,
+			pre_threshold_value = 220,
+			label = 'Stage'
+		)
+
+		self.mask_matching = IkaMatcher(
+			826, 37, 280, 34,
+			img_file = 'masks/ui_lobby_public2.png',
+			threshold = 0.97,
+			orig_threshold = 0.5,
+			pre_threshold_value = 220,
+			label = 'Matching',
+		)
+
+		self.mask_matched = IkaMatcher(
+			826, 37, 280, 34,
+			img_file = 'masks/ui_lobby_public_matched.png',
+			threshold = 0.97,
+			orig_threshold = 0.5,
+			pre_threshold_value = 220,
+			label = 'Matched',
+			debug = True
+		)
+
+if __name__ == "__main__":
+	target = cv2.imread(sys.argv[1])
+	obj = IkaScene_Lobby()
+
+	context = {
+		'engine': { 'frame': target },
+		'game': {},
+	}
+
+	matched = obj.match(context)
+	print("matched %s" % (matched))
+	print(context['game'])
+
+	cv2.waitKey(10000)

commit 4fe072981a8040c84bbaebe09d925cda0edd6532

IkaEngine: Fixed a bug introduced in commit e423104f8a80dba76affbe873324d147a7642362
Signed-off-by: Takeshi HASEGAWA <hasegaw@gmail.com>

IkaEngine.py
@@ -101,7 +101,7 @@ class IkaEngine:
 
 			# 死亡状態（「復活まであとｎ秒」）
 			if self.scn_ingame.matchDead(context):
-				if last_dead + 3 < time.time():
+				if self.last_dead + 3 < time.time():
 					self.callPlugins('onGameDead')
 				last_dead = time.time()
 