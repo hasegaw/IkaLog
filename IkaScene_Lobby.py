e76d1933772560a7beeb2c5ed63c24c89aa43546
Takeshi HASEGAWA <hasegaw@gmail.com>
2015-09-11 00:13:40 +0900

    IkaScene_Lobby: Added tag match recoginization
    Signed-off-by: Takeshi HASEGAWA <hasegaw@gmail.com>

--- IkaScene_Lobby.py
+++ IkaScene_Lobby.py
@@ -23,31 +23,44 @@ import sys
 from IkaUtils import *
 
 class IkaScene_Lobby:
+
 	def match(self, context):
 		frame = context['engine']['frame']
 
 		# 「ルール」「ステージ」の文字があればパブリックロビーで待ち合わせ中
-		if not self.mask_rule.match(frame):
-			return False
+#		if not self.mask_rule.match(frame):
+#			return False
 
-		if not self.mask_stage.match(frame):
-			return False
+#		if not self.mask_stage.match(frame):
+#			return False
 
 		# マッチング状況はどちらかがあるはず
-		r_matching = self.mask_matching.match(frame)
-		r_matched  = self.mask_matched.match(frame)
+		r_pub_matching = self.mask_matching.match(frame)
+		r_pub_matched  = self.mask_matched.match(frame)
+		r_tag_matching = self.mask_tag_matching.match(frame)
+		r_tag_matched  = self.mask_tag_matched.match(frame)
 
-		context['game']['lobby'] = {}
-		context['game']['lobby']['public'] = True
-		context['game']['lobby']['state'] = None
-
-		if not (r_matching or r_matched):
+		if not (r_pub_matching or r_pub_matched or r_tag_matching or r_tag_matched):
 			return False
 
-		if (r_matching):
+		context['game']['lobby'] = {
+			'type': None,
+			'state': None,
+		}
+
+		if (r_pub_matching or r_pub_matched):
+			context['game']['lobby']['type'] = 'public'
+
+		if (r_tag_matching or r_tag_matched):
+			context['game']['lobby']['type'] = 'tag'
+
+		if (r_pub_matching or r_tag_matching):
 			context['game']['lobby']['state'] = 'matching'
-		if (r_matched):
+
+		if (r_pub_matched or r_tag_matched):
 			context['game']['lobby']['state'] = 'matched'
+
+		#print(context['game']['lobby'])
 
 		return True
 
@@ -86,6 +99,25 @@ class IkaScene_Lobby:
 			orig_threshold = 0.5,
 			pre_threshold_value = 220,
 			label = 'Matched',
+			debug = True
+		)
+
+		self.mask_tag_matched = IkaMatcher(
+			826, 24, 280, 34,
+			img_file = 'masks/ui_lobby_tag_matched.png',
+			threshold = 0.90,
+			orig_threshold = 0.5,
+			pre_threshold_value = 210,
+			label = 'TagMatched',
+		)
+
+		self.mask_tag_matching = IkaMatcher(
+			826, 24, 280, 34,
+			img_file = 'masks/ui_lobby_tag_matching.png',
+			threshold = 0.97,
+			orig_threshold = 0.5,
+			pre_threshold_value = 210,
+			label = 'TagMatching',
 		)
 
 if __name__ == "__main__":
