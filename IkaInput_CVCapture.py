5f8546faa591e165dbe709b1826b579f407923c6
Takeshi HASEGAWA <hasegaw@gmail.com>
2015-09-18 03:07:57 +0900

    IkaInput_CVCapture: DirectShow Enumelation. Use Amarec Live! by default.
    - Added ability to enumerate DirectShow devices.
    - startCamera() supports either of device index or device name
    - In GUI mode, use AmarecTV Live! as default input
    - Updated __main__ to allow finding device name/number, and test the configuration
    
    videoinput.dll is renamed version of videoInput by Theodore Watson and Mihail Ivanchev.
    IkaLog project is going to use Mihails version as DLL library.
    https://github.com/MIvanchev/videoInput
    https://github.com/ofTheo/videoInput
    
    Signed-off-by: Takeshi HASEGAWA <hasegaw@gmail.com>

--- IkaInput_CVCapture.py
+++ IkaInput_CVCapture.py
@@ -21,6 +21,7 @@ import numpy as np
 import cv2
 import sys
 import os
+import ctypes
 import time
 import threading
 
@@ -31,6 +32,58 @@ try:
 	import wx
 except:
 	pass
+
+class InputSourceEnumerator:
+	def EnumWindows(self):
+		numDevices = ctypes.c_int(0)
+		r = self.dll.VI_Init()
+		if (r != 0):
+			return None
+
+		r = self.dll.VI_GetDeviceNames(ctypes.pointer(numDevices))
+		list = []
+		for n in range(numDevices.value):
+			friendly_name = self.dll.VI_GetDeviceName(n)
+			list.append(friendly_name)
+
+		self.dll.VI_Deinit()
+
+		return list
+
+	def EnumDummy(self):
+		cameras = []
+		for i in range(10):
+			cameras.append('Input source %d' % (i + 1))
+
+		return cameras
+
+	def Enumerate(self):
+		if IkaUtils.isWindows():
+			try:
+				return self.EnumWindows()
+			except:
+				IkaUtils.dprint('%s: Failed to enumerate DirectShow devices' % self)
+
+		return self.EnumDummy()
+
+	def __init__(self):
+		if IkaUtils.isWindows():
+			try:
+				self.c_int_p = ctypes.POINTER(ctypes.c_int)
+
+				ctypes.cdll.LoadLibrary('videoinput.dll')
+				self.dll = ctypes.CDLL('videoinput.dll')
+
+				self.dll.VI_Init.argtypes = []
+				self.dll.VI_Init.restype = ctypes.c_int
+				self.dll.VI_GetDeviceName.argtypes = [ctypes.c_int]
+				self.dll.VI_GetDeviceName.restype = ctypes.c_char_p
+				self.dll.VI_GetDeviceNames.argtypes = [self.c_int_p]
+				self.dll.VI_GetDeviceNames.restype = ctypes.c_char_p
+				self.dll.VI_GetDeviceName.argtypes = []
+			except:
+				IkaUtils.dprint("%s: Failed to initalize videoinput.dll" % self)
+
 
 class IkaInput_CVCapture:
 	cap = None
@@ -43,12 +96,18 @@ class IkaInput_CVCapture:
 
 	_systime_launch = int(time.time() * 1000)
 
-	source = ''
-	CameraIndex = 0
+	## アマレコTV のキャプチャデバイス名
+	DEV_AMAREC = "AmaRec Video Capture"
+
+	source = 'amarec'
+	SourceDevice = None
 	Deinterlace = False
 	File = ''
 
 	lock = threading.Lock()
+
+	def enumerateInputSources(self):
+		return InputSourceEnumerator().Enumerate()
 
 	def read(self):
 		if self.cap is None:
@@ -120,8 +179,24 @@ class IkaInput_CVCapture:
 
 		return False
 
-	def startCamera(self, source):
-		IkaUtils.dprint('%s: initalizing capture device %d' % (self, source))
+	def startCamera(self, source_name):
+
+		try:
+			source = int(source_name)
+		except:
+			IkaUtils.dprint('%s: Looking up device name %s' % (self, source_name))
+			try:
+				source_name = source_name.encode('utf-8')
+			except:
+				pass
+
+			try:
+				source = self.enumerateInputSources().index(source_name)
+			except:
+				IkaUtils.dprint("%s: Input '%s' not found" % (self, source_name))
+				return False
+
+		IkaUtils.dprint('%s: initalizing capture device %s' % (self, source))
 		self.realtime = True
 		if self.isWindows():
 			self.initCapture(700 + source)
@@ -134,20 +209,25 @@ class IkaInput_CVCapture:
 		self.initCapture(file)
 
 	def restartInput(self):
-		print(self.source, self.File, self.CameraIndex)
+		IkaUtils.dprint('RestartInput: source %s file %s device %s' % (self.source, self.File, self.SourceDevice))
+
 		if self.source == 'camera':
-			self.initCapture(self.CameraIndex)
+			self.startCamera(self.SourceDevice)
+
 		elif self.source == 'file':
 			self.startRecordedFile(self.File)
 		else:
-			self.cap = None
-			IkaUtils.dprint('No input is set up.')
+			# Use amarec if available
+			self.source = 'amarec'
+
+		if self.source == 'amarec':
+			self.startCamera(self.DEV_AMAREC)
 
 		success = True
 		if self.cap is None:
 			success = False
 
-		if not success:
+		if success:
 			if not self.cap.isOpened():
 				success = False
 
@@ -155,14 +235,15 @@ class IkaInput_CVCapture:
 
 	def ApplyUI(self):
 		self.source = ''
-		for control in [self.radioCamera, self.radioFile]:
+		for control in [self.radioAmarecTV, self.radioCamera, self.radioFile]:
 			if control.GetValue():
 				self.source = {
+					self.radioAmarecTV: 'amarec',
 					self.radioCamera: 'camera',
 					self.radioFile: 'file',
 				}[control]
 
-		self.CameraIndex = self.listCameras.GetSelection()
+		self.SourceDevice = self.listCameras.GetItems()[self.listCameras.GetSelection()]
 		self.File        = self.editFile.GetValue()
 		self.Deinterlace = self.checkDeinterlace.GetValue()
 
@@ -176,13 +257,21 @@ class IkaInput_CVCapture:
 			IkaUtils.dprint("%s: activated new input source" % self)
 
 	def RefreshUI(self):
+		if self.source == 'amarec':
+			self.radioAmarecTV.SetValue(True)
+
 		if self.source == 'camera':
 			self.radioCamera.SetValue(True)
 
 		if self.source == 'file':
 			self.radioFile.SetValue(True)
 
-		self.listCameras.SetSelection(self.CameraIndex)
+		try:
+			dev = self.SourceDevice
+			index = self.listCameras.GetItems().index(dev)
+			self.listCameras.SetSelection(index)
+		except:
+			IkaUtils.dprint('Current configured device is not in list')
 
 		if not self.File is None:
 			self.editFile.SetValue('')
@@ -199,7 +288,6 @@ class IkaInput_CVCapture:
 		self.onConfigReset(context)
 		try:
 			conf = context['config']['cvcapture']
-			print(context['config']['cvcapture'])
 		except:
 			conf = {}
 
@@ -210,12 +298,12 @@ class IkaInput_CVCapture:
 		except:
 			pass
 
-		if 'CameraIndex' in conf:
+		if 'SourceDevice' in conf:
 			try:
-				self.CameraIndex = int(conf['CameraIndex'])
+				self.SourceDevice = conf['SourceDevice']
 			except:
 				# FIXME
-				self.CameraIndex = 0
+				self.SourceDevice = 0
 
 		if 'File' in conf:
 			self.File = conf['File']
@@ -224,40 +312,49 @@ class IkaInput_CVCapture:
 			self.Deinterlace = conf['Deinterlace']
 
 		self.RefreshUI()
-
-		if self.source != '':
-			return self.restartInput()
-
-		return False
+		return self.restartInput()
 
 	def onConfigSaveToContext(self, context):
 		context['config']['cvcapture'] = {
 			'Source': self.source,
 			'File': self.File,
-			'CameraIndex': self.CameraIndex,
+			'SourceDevice': self.SourceDevice,
 			'Deinterlace': self.Deinterlace,
 		}
-		print(context['config']['cvcapture'])
 
 	def onConfigApply(self, context):
 		self.ApplyUI()
 
+	def OnReloadDevicesButtonClick(self, event = None):
+		cameras = self.enumerateInputSources()
+		self.listCameras.SetItems(cameras)
+		try:
+			index = self.enumerateInputSources().index(self.SourceDevice)
+			self.listCameras.SetSelection(index)
+		except:
+			IkaUtils.dprint('Error: Device not found')
+
 	def onOptionTabCreate(self, notebook):
-		self.panel = wx.Panel(notebook, wx.ID_ANY, size = (640, 360))
+		self.panel = wx.Panel(notebook, wx.ID_ANY)
 		self.page = notebook.InsertPage(0, self.panel, 'Input')
 
-		cameras = [ 'a', 'b' ]
+		cameras = self.enumerateInputSources()
 
 		self.layout = wx.BoxSizer(wx.VERTICAL)
 		self.panel.SetSizer(self.layout)
+		self.radioAmarecTV = wx.RadioButton(self.panel, wx.ID_ANY, u'Capture through AmarecTV')
+		self.radioAmarecTV.SetValue(True)
+
 		self.radioCamera = wx.RadioButton(self.panel, wx.ID_ANY, u'Realtime Capture from HDMI grabber')
 		self.radioFile = wx.RadioButton(self.panel, wx.ID_ANY, u'Read from pre-recorded video file (for testing)')
 		self.editFile = wx.TextCtrl(self.panel, wx.ID_ANY, u'hoge')
 		self.listCameras = wx.ListBox(self.panel, wx.ID_ANY, choices = cameras)
+		self.listCameras.SetSelection(0)
 		self.buttonReloadDevices = wx.Button(self.panel, wx.ID_ANY, u'Reload Devices')
 		self.checkDeinterlace = wx.CheckBox(self.panel, wx.ID_ANY, u'Enable Deinterlacing (experimental)')
 
 		self.layout.Add(wx.StaticText(self.panel, wx.ID_ANY, u'Select Input source:'))
+		self.layout.Add(self.radioAmarecTV)
 		self.layout.Add(self.radioCamera)
 		self.layout.Add(self.listCameras, flag= wx.EXPAND)
 		self.layout.Add(self.buttonReloadDevices)
@@ -266,9 +363,18 @@ class IkaInput_CVCapture:
 		self.layout.Add(self.checkDeinterlace)
 		self.layout.Add(wx.StaticText(self.panel, wx.ID_ANY, u'Video Offset'))
 
+		self.buttonReloadDevices.Bind(wx.EVT_BUTTON, self.OnReloadDevicesButtonClick)
+
 if __name__ == "__main__":
 	obj = IkaInput_CVCapture()
-	obj.startCamera(0)
+
+	list = InputSourceEnumerator().Enumerate()
+	for n in range(len(list)):
+		print("%d: %s" % (n, list[n]))
+
+	dev = input("Please input number (or name) of capture device: ")
+
+	obj.startCamera(dev)
 
 	k = 0
 	while k != 27:
