73cce71bfb3d68b47fcc24248e574197ee10554c
Takeshi HASEGAWA <hasegaw@gmail.com>
2015-09-23 02:23:49 +0900

    IkaGlyphRecoginizer: autopep8
    Signed-off-by: Takeshi HASEGAWA <hasegaw@gmail.com>

--- IkaGlyphRecoginizer.py
+++ IkaGlyphRecoginizer.py
@@ -24,306 +24,306 @@ import numpy as np
 import os
 import pickle
 
-## @package IkaGlyphRecoginizer
+# @package IkaGlyphRecoginizer
+
 
 class IkaGlyphRecoginizer:
-	## Maximum Hue Value in OpenCV HSV Color.
-	_HSV_COLOR_MAX = 185 
+    # Maximum Hue Value in OpenCV HSV Color.
+    _HSV_COLOR_MAX = 185
 
-	## Number of Hue samples
-	_HSV_COLOR_SAMPLES = 36
+    # Number of Hue samples
+    _HSV_COLOR_SAMPLES = 36
 
-	## Models
-	models = []
+    # Models
+    models = []
 
-	## pre-calculated Hue samples
+    # pre-calculated Hue samples
 
-	def calcurateHueSamples(self, num_colors):
-		samples = []
-		for i in range(num_colors):
-			c_min = self._HSV_COLOR_MAX / num_colors * i
-			c_max = self._HSV_COLOR_MAX / num_colors * (i + 1)
-			if c_min == 0:
-				c_min = 1
-			samples.append((c_min, c_max))
-		return samples
+    def calcurateHueSamples(self, num_colors):
+        samples = []
+        for i in range(num_colors):
+            c_min = self._HSV_COLOR_MAX / num_colors * i
+            c_max = self._HSV_COLOR_MAX / num_colors * (i + 1)
+            if c_min == 0:
+                c_min = 1
+            samples.append((c_min, c_max))
+        return samples
 
-	## Normalize the image. (for weapons)
-	#
-	# - Crop the image
-	# - Detect background color
-	# - Replace the background color to black
-	# - Replace white color to blue (0, 0, 255) because we want to compare using Hue value.
-	#
-	# @param img    the source image
-	# @return (img,out_img)  the result
-	def normalizeWeaponImage(self, img):
-		h = img.shape[0]
-		w = img.shape[1]
-		img = img[2:h - 4, 5:w - 3]
+    # Normalize the image. (for weapons)
+    #
+    # - Crop the image
+    # - Detect background color
+    # - Replace the background color to black
+    # - Replace white color to blue (0, 0, 255) because we want to compare using Hue value.
+    #
+    # @param img    the source image
+    # @return (img,out_img)  the result
+    def normalizeWeaponImage(self, img):
+        h = img.shape[0]
+        w = img.shape[1]
+        img = img[2:h - 4, 5:w - 3]
 
-		out_img = img.copy()
-		h = img.shape[0]
-		w = img.shape[1]
+        out_img = img.copy()
+        h = img.shape[0]
+        w = img.shape[1]
 
-		img_hsv = cv2.cvtColor(out_img, cv2.COLOR_BGR2HSV)
-	
-		bgcolor_sample = img_hsv[h - 3:h, 0:3, 0] # Hue
-		bg_h_color = np.average(bgcolor_sample)
-		rad = 7
-		bg_h_color1 = int(bg_h_color - rad)
-		bg_h_color2 = int(bg_h_color + rad)
+        img_hsv = cv2.cvtColor(out_img, cv2.COLOR_BGR2HSV)
 
-		# デコ/カスタム要素で認識を優先するため画像で該当部分を拡大
-		x1 = 0
-		x2 = int(w * 0.7)
-		y1 = h - 10
-		out_img[y1: h -1, x1:w - 1, :] = cv2.resize(img[y1: h - 1, x2: w - 1], (w - x1 - 1, h - y1 - 1), interpolation = cv2.INTER_NEAREST)
-		img_hsv = cv2.cvtColor(out_img, cv2.COLOR_BGR2HSV)
-	
-		img_mask = cv2.inRange(img_hsv[:, :, 0], bg_h_color1, bg_h_color2)
-		img_mask2 = 255 - cv2.inRange(img_hsv[:, :, 2], 165,255) # Visibility
-		img_mask = np.minimum(img_mask, img_mask2)
-		
-		for i in range(3):
-			out_img[:, :, i] = np.minimum(out_img[:, :, i], 255 - img_mask)
-	
-		# 白いところを検出する (s が低く v が高い)
-		white_mask_s = cv2.inRange(img_hsv[:, :, 1], 0, 48)
-		white_mask_v = cv2.inRange(img_hsv[:, :, 2], 224, 256)
-		white_mask = np.minimum(white_mask_s, white_mask_v)
-	
-		# 白色を置きかえ
-		out_img[:, :, 0] = np.maximum(out_img[:, :, 0], white_mask)
-		out_img[:, :, 1] = np.minimum(out_img[:, :, 1], 255 - white_mask)
-		out_img[:, :, 2] = np.minimum(out_img[:, :, 2], 255 - white_mask)
-	
-		#cv2.imshow('orig', img)
-		#cv2.imshow('H', img_h)
-		#cv2.imshow('S', img_s)
-		#cv2.imshow('V', img_v)
-		#cv2.imshow('HSV', img_hsv)
-		#cv2.imshow('mask', img_mask)
-		#cv2.imshow('mask2', img_mask2)
-		#cv2.waitKey()
-		return [
-			out_img,
-			img,
-		]
-	
-	def countH(self, img_h, samples):
-		r = []
-		for sample in samples:
-			x = cv2.inRange(img_h, sample[0], sample[1])
-			cond = x > 127
-			r.append(len(np.extract(cond, img_h)))
-		return r
-	
-	
-	## Analyze a image.
-	#
-	def analyzeImage(self, img, blocks_x = 5, blocks_y = 5, debug = False):
-		samples = self._precalculatedHueSamples
+        bgcolor_sample = img_hsv[h - 3:h, 0:3, 0]  # Hue
+        bg_h_color = np.average(bgcolor_sample)
+        rad = 7
+        bg_h_color1 = int(bg_h_color - rad)
+        bg_h_color2 = int(bg_h_color + rad)
 
-		imgs = self.normalizeWeaponImage(img)
-		d = imgs[0]
-		bw = int(d.shape[1] / blocks_x)
-		bh = int(d.shape[0] / blocks_y)
-	
-		img_hsv = cv2.cvtColor(d, cv2.COLOR_BGR2HSV)
-		img_h = img_hsv[:, :, 0]
-		img_s = img_hsv[:, :, 1]
-		img_v = img_hsv[:, :, 2]
-	
-		hist = []
-		part_img_h = np.zeros((bh, bw), np.uint8)
-		part_img_s = np.zeros((bh, bw), np.uint8)
-	
-		for bx in range(blocks_x):
-			for by in range(blocks_y):
-				x1 = bw * (bx + 0)
-				x2 = bw * (bx + 1)
-				y1 = bh * (by + 0)
-				y2 = bh * (by + 1)
-				part_img_h[:, :] = img_h[y1:y2, x1:x2]
-				part_img_s[:, :] = img_s[y1:y2, x1:x2]
-				hist.extend(self.countH(part_img_h, samples))
-	
-		param = { 'hist': np.array(hist).astype(dtype = np.float) }
-		if (not debug):
-			return param
-	
-		offset_w = 100#img_h.shape[1]
-		new_w = offset_w + (len(hist) * 8)
-		new_h = img.shape[0]
-		dimg = cv2.resize(img, (new_w, new_h))
-		dimg.fill(0)	
-		dimg = cv2.cvtColor(dimg, cv2.COLOR_BGR2HSV)
-	
-		# カラーバーを書く
-		for i in range(len(hist)):
-			sample = samples[i % len(samples)]		
-			c_avg = int((sample[0] + sample[1]) / 2)
-	
-			y1 = new_h - hist[i]
-			y2 = new_h
-			x1 = offset_w + 8 * (i + 0) 
-			x2 = offset_w + 8 * (i + 1) 
-	
-			dimg[:, x1:x2, 0].fill(c_avg)
-			dimg[:, x1:x2, 1].fill(255)
-			dimg[:, x1:x2, 2].fill(0)
-			dimg[y1:y2, x1:x2, 2].fill(255)
-	
-		dimg = cv2.cvtColor(dimg, cv2.COLOR_HSV2BGR)
-		w = imgs[0].shape[1]
-		h = imgs[0].shape[0]
-		dimg[:h,:w] = imgs[0][:,:]
-		dimg[:h,w:w*2] = imgs[1][:,:]
-		cv2.imshow(':D', dimg)
-		#cv2.waitKey(3000)
-	
-		return param, dimg
-	
-	
-	def calculateParamersFromSamples(self, l_hist):
-		num_samples = len(l_hist)
-		colors = len(l_hist[0])
-	
-		a = np.array(l_hist).astype(dtype = np.float)
-	#	print("num_samples = %d, colors = %d" % (num_samples, colors))
-		#print(a)
-		#print(len(a[:,1]))
-	
-		b = a.T
-		#print(len(b[:,1]))
-	
-		h_avg = np.average(b, axis = 1)
-		h_var = np.var(b, axis = 1)
-		from pprint import pprint
-		#pprint(h_avg)
-		#pprint(h_var)
-	
-		for e in a:
-			abs_val = np.abs(e - h_avg)
-			cond = (abs_val < h_var)
-	#		print(cond)
-	
-			n = len(np.extract(cond, cond))
-	#		print(n)
-	
-		return { 'h_avg': h_avg, 'h_var': h_var }
-	
-	def matchImageWithParameter(self, img_param, trained):
-		h_avg = trained['h_avg']
-		h_var = trained['h_var']
-	
-		abs_val = np.abs(img_param['hist'] - h_avg)
-	
-		cond0 = (h_var < 50 )
-		cond1 = (abs_val < (h_var))
-		cond = np.logical_and(cond0, cond1)
-	
-		# 分散を使用した重み付け
-		scores = np.minimum(1000, (abs_val / (h_var + 0.1))) * h_avg
-		score = np.sum(np.extract(cond, scores))
-		scores_max = np.sum(np.extract(cond0, scores))
-		score_normalized = (score * 100) / scores_max
-	
-		return score_normalized
-		cv2.imshow(':D', img)
-		#cv2.waitKey(1000)
-	
-	def showLearnedWeaponImage(self, l, name = 'hoge', save = None):
-		new_h = l[0].shape[0] * len(l)
-		new_w = l[0].shape[1]
-	
-		dest = cv2.resize(l[0], (new_w, new_h))
-		dest.fill(0)
-		y = 0
-		for i in l:
-			h = i.shape[0]
-			dest[y:y + h] = i[0: h]
-			y = y + h
-	
-		cv2.imshow(name, dest)
-		if save:
-			cv2.imwrite(save, dest)
+        # デコ/カスタム要素で認識を優先するため画像で該当部分を拡大
+        x1 = 0
+        x2 = int(w * 0.7)
+        y1 = h - 10
+        out_img[y1: h - 1, x1:w - 1, :] = cv2.resize(
+            img[y1: h - 1, x2: w - 1], (w - x1 - 1, h - y1 - 1), interpolation=cv2.INTER_NEAREST)
+        img_hsv = cv2.cvtColor(out_img, cv2.COLOR_BGR2HSV)
 
-	def testModel(self, model):
-		scores = []
-		for sample in model['samples']:
-			score = self.guessImage1(img = sample, model = model)
-			try:
-				scores.append(int(score * 10) / 10)
-			except:
-				pass
+        img_mask = cv2.inRange(img_hsv[:, :, 0], bg_h_color1, bg_h_color2)
+        img_mask2 = 255 - cv2.inRange(img_hsv[:, :, 2], 165, 255)  # Visibility
+        img_mask = np.minimum(img_mask, img_mask2)
 
-		print("%s %s" % (model['name'], scores))
+        for i in range(3):
+            out_img[:, :, i] = np.minimum(out_img[:, :, i], 255 - img_mask)
 
-	def learnImageGroup(self, name = None, dir = None):
-		l = []
-		l_hist = []
-		samples = []
-		for root, dirs, files in os.walk(dir):
-			for file in files:
-				if file.endswith(".png"):
-					f = os.path.join(root, file)
-					img = cv2.imread(f)
-					samples.append(img)
+        # 白いところを検出する (s が低く v が高い)
+        white_mask_s = cv2.inRange(img_hsv[:, :, 1], 0, 48)
+        white_mask_v = cv2.inRange(img_hsv[:, :, 2], 224, 256)
+        white_mask = np.minimum(white_mask_s, white_mask_v)
 
-					param, r = self.analyzeImage(img, debug = True)
-					l.append(r)
-					l_hist.append(param['hist'])
-	
-		self.showLearnedWeaponImage(l, name)
-		trained = self.calculateParamersFromSamples(l_hist)
-		model = { 'name': name, 'model': trained, 'samples': samples }
+        # 白色を置きかえ
+        out_img[:, :, 0] = np.maximum(out_img[:, :, 0], white_mask)
+        out_img[:, :, 1] = np.minimum(out_img[:, :, 1], 255 - white_mask)
+        out_img[:, :, 2] = np.minimum(out_img[:, :, 2], 255 - white_mask)
 
-		self.models.append(model)
-	
-		return model 
-	
-	def guessImage1(self, img = None, img_param = None, model = None):
-		if img_param is None:
-			img_param = self.analyzeImage(img)
-	
-		return self.matchImageWithParameter(img_param, model['model'])
-	
-	def guessImage(self, img = None, img_param = None, models = None):
-		if models is None:
-			models = self.models
+        #cv2.imshow('orig', img)
+        #cv2.imshow('H', img_h)
+        #cv2.imshow('S', img_s)
+        #cv2.imshow('V', img_v)
+        #cv2.imshow('HSV', img_hsv)
+        #cv2.imshow('mask', img_mask)
+        #cv2.imshow('mask2', img_mask2)
+        # cv2.waitKey()
+        return [
+            out_img,
+            img,
+        ]
 
-		if img_param is None:
-			img_param = self.analyzeImage(img)
-	
-		result = { 'name': None, 'score': 0 }
-		for model in models:
-			s = self.guessImage1(img_param = img_param, model = model)
-			if s > result['score']:
-				result['score'] = s
-				result['name'] = model['name']
-			model_matched = model
-		return (result, model)
+    def countH(self, img_h, samples):
+        r = []
+        for sample in samples:
+            x = cv2.inRange(img_h, sample[0], sample[1])
+            cond = x > 127
+            r.append(len(np.extract(cond, img_h)))
+        return r
 
-	def stripModel(self):
-		models = self.models.copy()
-		for model in models:
-			if "samples" in model:
-				del model["samples"]
-		return models
+    # Analyze a image.
+    #
+    def analyzeImage(self, img, blocks_x=5, blocks_y=5, debug=False):
+        samples = self._precalculatedHueSamples
 
-	def saveModelToFile(self, file):
-		f = open(file, "wb")
-		pickle.dump([self.stripModel()], f)
-		f.close()
+        imgs = self.normalizeWeaponImage(img)
+        d = imgs[0]
+        bw = int(d.shape[1] / blocks_x)
+        bh = int(d.shape[0] / blocks_y)
 
-	def loadModelFromFile(self, file):
-		f = open(file, "rb")
-		l = pickle.load(f)
-		f.close()
-		self.models = l[0]
+        img_hsv = cv2.cvtColor(d, cv2.COLOR_BGR2HSV)
+        img_h = img_hsv[:, :, 0]
+        img_s = img_hsv[:, :, 1]
+        img_v = img_hsv[:, :, 2]
 
-	def __init__(self):
-		self._precalculatedHueSamples = self.calcurateHueSamples(self._HSV_COLOR_SAMPLES)
+        hist = []
+        part_img_h = np.zeros((bh, bw), np.uint8)
+        part_img_s = np.zeros((bh, bw), np.uint8)
 
+        for bx in range(blocks_x):
+            for by in range(blocks_y):
+                x1 = bw * (bx + 0)
+                x2 = bw * (bx + 1)
+                y1 = bh * (by + 0)
+                y2 = bh * (by + 1)
+                part_img_h[:, :] = img_h[y1:y2, x1:x2]
+                part_img_s[:, :] = img_s[y1:y2, x1:x2]
+                hist.extend(self.countH(part_img_h, samples))
+
+        param = {'hist': np.array(hist).astype(dtype=np.float)}
+        if (not debug):
+            return param
+
+        offset_w = 100  # img_h.shape[1]
+        new_w = offset_w + (len(hist) * 8)
+        new_h = img.shape[0]
+        dimg = cv2.resize(img, (new_w, new_h))
+        dimg.fill(0)
+        dimg = cv2.cvtColor(dimg, cv2.COLOR_BGR2HSV)
+
+        # カラーバーを書く
+        for i in range(len(hist)):
+            sample = samples[i % len(samples)]
+            c_avg = int((sample[0] + sample[1]) / 2)
+
+            y1 = new_h - hist[i]
+            y2 = new_h
+            x1 = offset_w + 8 * (i + 0)
+            x2 = offset_w + 8 * (i + 1)
+
+            dimg[:, x1:x2, 0].fill(c_avg)
+            dimg[:, x1:x2, 1].fill(255)
+            dimg[:, x1:x2, 2].fill(0)
+            dimg[y1:y2, x1:x2, 2].fill(255)
+
+        dimg = cv2.cvtColor(dimg, cv2.COLOR_HSV2BGR)
+        w = imgs[0].shape[1]
+        h = imgs[0].shape[0]
+        dimg[:h, :w] = imgs[0][:, :]
+        dimg[:h, w:w * 2] = imgs[1][:, :]
+        cv2.imshow(':D', dimg)
+        # cv2.waitKey(3000)
+
+        return param, dimg
+
+    def calculateParamersFromSamples(self, l_hist):
+        num_samples = len(l_hist)
+        colors = len(l_hist[0])
+
+        a = np.array(l_hist).astype(dtype=np.float)
+    #	print("num_samples = %d, colors = %d" % (num_samples, colors))
+        # print(a)
+        # print(len(a[:,1]))
+
+        b = a.T
+        # print(len(b[:,1]))
+
+        h_avg = np.average(b, axis=1)
+        h_var = np.var(b, axis=1)
+        from pprint import pprint
+        # pprint(h_avg)
+        # pprint(h_var)
+
+        for e in a:
+            abs_val = np.abs(e - h_avg)
+            cond = (abs_val < h_var)
+    #		print(cond)
+
+            n = len(np.extract(cond, cond))
+    #		print(n)
+
+        return {'h_avg': h_avg, 'h_var': h_var}
+
+    def matchImageWithParameter(self, img_param, trained):
+        h_avg = trained['h_avg']
+        h_var = trained['h_var']
+
+        abs_val = np.abs(img_param['hist'] - h_avg)
+
+        cond0 = (h_var < 50)
+        cond1 = (abs_val < (h_var))
+        cond = np.logical_and(cond0, cond1)
+
+        # 分散を使用した重み付け
+        scores = np.minimum(1000, (abs_val / (h_var + 0.1))) * h_avg
+        score = np.sum(np.extract(cond, scores))
+        scores_max = np.sum(np.extract(cond0, scores))
+        score_normalized = (score * 100) / scores_max
+
+        return score_normalized
+        cv2.imshow(':D', img)
+        # cv2.waitKey(1000)
+
+    def showLearnedWeaponImage(self, l, name='hoge', save=None):
+        new_h = l[0].shape[0] * len(l)
+        new_w = l[0].shape[1]
+
+        dest = cv2.resize(l[0], (new_w, new_h))
+        dest.fill(0)
+        y = 0
+        for i in l:
+            h = i.shape[0]
+            dest[y:y + h] = i[0: h]
+            y = y + h
+
+        cv2.imshow(name, dest)
+        if save:
+            cv2.imwrite(save, dest)
+
+    def testModel(self, model):
+        scores = []
+        for sample in model['samples']:
+            score = self.guessImage1(img=sample, model=model)
+            try:
+                scores.append(int(score * 10) / 10)
+            except:
+                pass
+
+        print("%s %s" % (model['name'], scores))
+
+    def learnImageGroup(self, name=None, dir=None):
+        l = []
+        l_hist = []
+        samples = []
+        for root, dirs, files in os.walk(dir):
+            for file in files:
+                if file.endswith(".png"):
+                    f = os.path.join(root, file)
+                    img = cv2.imread(f)
+                    samples.append(img)
+
+                    param, r = self.analyzeImage(img, debug=True)
+                    l.append(r)
+                    l_hist.append(param['hist'])
+
+        self.showLearnedWeaponImage(l, name)
+        trained = self.calculateParamersFromSamples(l_hist)
+        model = {'name': name, 'model': trained, 'samples': samples}
+
+        self.models.append(model)
+
+        return model
+
+    def guessImage1(self, img=None, img_param=None, model=None):
+        if img_param is None:
+            img_param = self.analyzeImage(img)
+
+        return self.matchImageWithParameter(img_param, model['model'])
+
+    def guessImage(self, img=None, img_param=None, models=None):
+        if models is None:
+            models = self.models
+
+        if img_param is None:
+            img_param = self.analyzeImage(img)
+
+        result = {'name': None, 'score': 0}
+        for model in models:
+            s = self.guessImage1(img_param=img_param, model=model)
+            if s > result['score']:
+                result['score'] = s
+                result['name'] = model['name']
+            model_matched = model
+        return (result, model)
+
+    def stripModel(self):
+        models = self.models.copy()
+        for model in models:
+            if "samples" in model:
+                del model["samples"]
+        return models
+
+    def saveModelToFile(self, file):
+        f = open(file, "wb")
+        pickle.dump([self.stripModel()], f)
+        f.close()
+
+    def loadModelFromFile(self, file):
+        f = open(file, "rb")
+        l = pickle.load(f)
+        f.close()
+        self.models = l[0]
+
+    def __init__(self):
+        self._precalculatedHueSamples = self.calcurateHueSamples(
+            self._HSV_COLOR_SAMPLES)
