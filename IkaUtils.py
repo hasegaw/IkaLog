a1f9f10b9be9757901da7e76ddcdd409f42b2c61
Takeshi HASEGAWA <hasegaw@gmail.com>
2015-09-23 02:22:52 +0900

    IkaUtils: autopep8
    Signed-off-by: Takeshi HASEGAWA <hasegaw@gmail.com>

--- IkaUtils.py
+++ IkaUtils.py
@@ -25,249 +25,258 @@ import sys
 import os
 import re
 
+
 class IkaUtils:
-	@staticmethod
-	def isWindows():
-		try:
-			os.uname()
-		except AttributeError:
-			return True
-		return False
 
-	@staticmethod
-	def dprint(text):
-		print(text, file = sys.stderr)
+    @staticmethod
+    def isWindows():
+        try:
+            os.uname()
+        except AttributeError:
+            return True
+        return False
 
-	## Find the local player.
-	#
-	# @param context   IkaLog Context.
-	# @return The player information (Directionary class) if found.
-	@staticmethod
-	def getMyEntryFromContext(context):
-		for e in context['game']['players']:
-			if e['me']:
-				return e
-		return None
+    @staticmethod
+    def dprint(text):
+        print(text, file=sys.stderr)
 
-	## Get player's title.
-	#
-	# @param playerEntry The player.
-	# @return Title in string. Returns None if playerEntry doesn't have title data.
-	@staticmethod
-	def playerTitle(playerEntry):
-		if playerEntry is None:
-			return None
+    # Find the local player.
+    #
+    # @param context   IkaLog Context.
+    # @return The player information (Directionary class) if found.
+    @staticmethod
+    def getMyEntryFromContext(context):
+        for e in context['game']['players']:
+            if e['me']:
+                return e
+        return None
 
-		if not (('gender' in playerEntry) and ('prefix' in playerEntry)):
-			return None
+    # Get player's title.
+    #
+    # @param playerEntry The player.
+    # @return Title in string. Returns None if playerEntry doesn't have title data.
+    @staticmethod
+    def playerTitle(playerEntry):
+        if playerEntry is None:
+            return None
 
-		prefix = re.sub('の', '', playerEntry['prefix'])
-		return "%s%s" % (prefix, playerEntry['gender'])
+        if not (('gender' in playerEntry) and ('prefix' in playerEntry)):
+            return None
 
-	@staticmethod
-	def map2text(map, unknown = None, lang = "ja"):
-		if map is None:
-			if unknown is None:
-				unknown = "?"
-			return unknown
-		return map['name']
+        prefix = re.sub('の', '', playerEntry['prefix'])
+        return "%s%s" % (prefix, playerEntry['gender'])
 
-	@staticmethod
-	def rule2text(rule, unknown = None, lang = "ja"):
-		if rule is None:
-			if unknown is None:
-				unknown = "?"
-			return unknown
-		return rule['name']
+    @staticmethod
+    def map2text(map, unknown=None, lang="ja"):
+        if map is None:
+            if unknown is None:
+                unknown = "?"
+            return unknown
+        return map['name']
 
-	@staticmethod
-	def cropImageGray(img, left, top, width, height):
-		if len(img.shape) > 2 and img.shape[2] != 1:
-			return cv2.cvtColor(
-					img[top:top + height, left:left + width],
-					cv2.COLOR_BGR2GRAY
-			)
-		return img[top:top + height, left:left + width]
+    @staticmethod
+    def rule2text(rule, unknown=None, lang="ja"):
+        if rule is None:
+            if unknown is None:
+                unknown = "?"
+            return unknown
+        return rule['name']
 
-	@staticmethod
-	def matchWithMask(img, mask, threshold = 99.0, orig_threshold = 70.0, debug = False):
-		if len(img.shape) > 2 and img.shape[2] != 1:
-			img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    @staticmethod
+    def cropImageGray(img, left, top, width, height):
+        if len(img.shape) > 2 and img.shape[2] != 1:
+            return cv2.cvtColor(
+                img[top:top + height, left:left + width],
+                cv2.COLOR_BGR2GRAY
+            )
+        return img[top:top + height, left:left + width]
 
-		# Check false-positive
-		orig_hist = cv2.calcHist([img], [0], None, [3], [0, 256])
-		match2 = orig_hist[2] / np.sum(orig_hist)
+    @staticmethod
+    def matchWithMask(img, mask, threshold=99.0, orig_threshold=70.0, debug=False):
+        if len(img.shape) > 2 and img.shape[2] != 1:
+            img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-		if match2 > orig_threshold:
-			# False-Positive condition.
-			#print("original %f > orig_threshold %f" % (match2, orig_threshold))
-			return False
+        # Check false-positive
+        orig_hist = cv2.calcHist([img], [0], None, [3], [0, 256])
+        match2 = orig_hist[2] / np.sum(orig_hist)
 
-		ret, thresh1 = cv2.threshold(img, 230, 255, cv2.THRESH_BINARY)
-		added = thresh1 + mask
-		hist = cv2.calcHist([added], [0], None, [3], [0, 256])
+        if match2 > orig_threshold:
+            # False-Positive condition.
+            #print("original %f > orig_threshold %f" % (match2, orig_threshold))
+            return False
 
-		match = hist[2] / np.sum(hist)
+        ret, thresh1 = cv2.threshold(img, 230, 255, cv2.THRESH_BINARY)
+        added = thresh1 + mask
+        hist = cv2.calcHist([added], [0], None, [3], [0, 256])
 
-		if debug and (match > threshold):
-			print("match2 %f match %f > threshold %f" % (match2, match, threshold))
-			cv2.imshow('match_img', img)
-			cv2.imshow('match_mask', mask)
-			cv2.imshow('match_added', added)
-			#cv2.waitKey()
+        match = hist[2] / np.sum(hist)
 
-		return match > threshold
+        if debug and (match > threshold):
+            print("match2 %f match %f > threshold %f" %
+                  (match2, match, threshold))
+            cv2.imshow('match_img', img)
+            cv2.imshow('match_mask', mask)
+            cv2.imshow('match_added', added)
+            # cv2.waitKey()
 
-	@staticmethod
-	def loadMask(file, left, top, width, height):
-		mask = cv2.imread(file)
-		if mask is None:
-			print("マスクデータ %s のロードに失敗しました")
-			# raise a exception
+        return match > threshold
 
-		mask = mask[top:top + height, left:left + width]
+    @staticmethod
+    def loadMask(file, left, top, width, height):
+        mask = cv2.imread(file)
+        if mask is None:
+            print("マスクデータ %s のロードに失敗しました")
+            # raise a exception
 
-		# BGR to GRAY
-		if mask.shape[2] > 1:
-			mask = cv2.cvtColor(mask, cv2.COLOR_BGR2GRAY)
+        mask = mask[top:top + height, left:left + width]
 
-		return mask 
+        # BGR to GRAY
+        if mask.shape[2] > 1:
+            mask = cv2.cvtColor(mask, cv2.COLOR_BGR2GRAY)
 
-	@staticmethod
-	def getWinLoseText(won, win_text = "勝ち", lose_text = "負け", unknown_text = "不明"):
-		if won is None:
-			return unknown_text
-		return win_text if won else lose_text
+        return mask
 
-	@staticmethod
-	def writeScreenshot(destfile, frame):
-		try:
-			cv2.imwrite(destfile, frame)
-			return os.path.isfile(destfile)
+    @staticmethod
+    def getWinLoseText(won, win_text="勝ち", lose_text="負け", unknown_text="不明"):
+        if won is None:
+            return unknown_text
+        return win_text if won else lose_text
 
-		except:
-			print("Screenshot: failed")
-			return False
+    @staticmethod
+    def writeScreenshot(destfile, frame):
+        try:
+            cv2.imwrite(destfile, frame)
+            return os.path.isfile(destfile)
+
+        except:
+            print("Screenshot: failed")
+            return False
 
 
-## Match images with mask data.
+# Match images with mask data.
 class IkaMatcher:
 
-	## Calculate false-postive score (masked area must be black)
-	@classmethod
-	def FP_BACK_IS_BLACK(self, mask_img = None, img_bgr = None, img_gray = None):
-		fp_img = np.minimum(img_gray, mask_img)
-		fp_img = cv2.inRange(fp_img, 16, 256)
-		orig_hist = cv2.calcHist([fp_img], [0], None, [3], [0, 256])
-		orig_raito  = orig_hist[2] / np.sum(orig_hist)
-		return orig_raito, fp_img
+    # Calculate false-postive score (masked area must be black)
+    @classmethod
+    def FP_BACK_IS_BLACK(self, mask_img=None, img_bgr=None, img_gray=None):
+        fp_img = np.minimum(img_gray, mask_img)
+        fp_img = cv2.inRange(fp_img, 16, 256)
+        orig_hist = cv2.calcHist([fp_img], [0], None, [3], [0, 256])
+        orig_raito = orig_hist[2] / np.sum(orig_hist)
+        return orig_raito, fp_img
 
-	## Calculate false-postive score (masked area is not white)
-	@classmethod
-	def FP_FRONT_IS_WHITE(self, mask_img = None, img_bgr = None, img_gray = None):
-		img_hsv = cv2.cvtColor(img_bgr, cv2.COLOR_BGR2HSV)
-		white_mask_s = cv2.inRange(img_hsv[:, :, 1], 0, 6)
-		white_mask_v = cv2.inRange(img_hsv[:, :, 2], 250, 256)
-		fp_img = np.minimum(mask_img, white_mask_s, white_mask_v) # マスク範囲=0, 白以外=0, 白=255
-		orig_hist = cv2.calcHist([fp_img], [0], None, [3], [0, 256])
-		orig_raito  = orig_hist[2] / np.sum(orig_hist)
-		return orig_raito, fp_img
+    # Calculate false-postive score (masked area is not white)
+    @classmethod
+    def FP_FRONT_IS_WHITE(self, mask_img=None, img_bgr=None, img_gray=None):
+        img_hsv = cv2.cvtColor(img_bgr, cv2.COLOR_BGR2HSV)
+        white_mask_s = cv2.inRange(img_hsv[:, :, 1], 0, 6)
+        white_mask_v = cv2.inRange(img_hsv[:, :, 2], 250, 256)
+        # マスク範囲=0, 白以外=0, 白=255
+        fp_img = np.minimum(mask_img, white_mask_s, white_mask_v)
+        orig_hist = cv2.calcHist([fp_img], [0], None, [3], [0, 256])
+        orig_raito = orig_hist[2] / np.sum(orig_hist)
+        return orig_raito, fp_img
 
-	## Match the image.
-	# @param self   The object.
-	# @param img    Frame data.
-	# @param debug  If true, show debug information.
-	def match(self, img, debug = None):
-		if debug is None:
-			debug = self.debug
+    # Match the image.
+    # @param self   The object.
+    # @param img    Frame data.
+    # @param debug  If true, show debug information.
+    def match(self, img, debug=None):
+        if debug is None:
+            debug = self.debug
 
-		# Crop
-		cropped = (img.shape[0] == self.height) and (img.shape[1] == self.width)
-		if not cropped:
-			img = img[self.top: self.top + self.height, self.left: self.left + self.width]
+        # Crop
+        cropped = (img.shape[0] == self.height) and (
+            img.shape[1] == self.width)
+        if not cropped:
+            img = img[self.top: self.top + self.height,
+                      self.left: self.left + self.width]
 
-		# Grayscale
-		if img.shape[2] > 1:
-			img_bgr = img
-			img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-		else:
-			img_bgr = None
-			img_gray = img
+        # Grayscale
+        if img.shape[2] > 1:
+            img_bgr = img
+            img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        else:
+            img_bgr = None
+            img_gray = img
 
-		# Check false-positive
-		#orig_raito  = self.FP_BACK_IS_BLACK(img_gray = img)
-		orig_raito,img_fp  = self.false_positive_method(mask_img = self.mask_img, img_bgr = img_bgr, img_gray = img_gray)
-		raito = 0
+        # Check false-positive
+        #orig_raito  = self.FP_BACK_IS_BLACK(img_gray = img)
+        orig_raito, img_fp = self.false_positive_method(
+            mask_img=self.mask_img, img_bgr=img_bgr, img_gray=img_gray)
+        raito = 0
 
-		match = True
+        match = True
 
-		if orig_raito > self.orig_threshold:
-			match = False
-			if debug:
-				pass
-				#print("original image exceeded orig_threshold")
+        if orig_raito > self.orig_threshold:
+            match = False
+            if debug:
+                pass
+                #print("original image exceeded orig_threshold")
 
-		if match and not (self.pre_threshold_value is None):
-			ret, img_gray = cv2.threshold(img_gray, self.pre_threshold_value, 255, cv2.THRESH_BINARY)
+        if match and not (self.pre_threshold_value is None):
+            ret, img_gray = cv2.threshold(
+                img_gray, self.pre_threshold_value, 255, cv2.THRESH_BINARY)
 
-		if match:
-			added = np.maximum(img_gray, self.mask_img)
+        if match:
+            added = np.maximum(img_gray, self.mask_img)
 
-			hist = cv2.calcHist([added], [0], None, [3], [0, 256])
+            hist = cv2.calcHist([added], [0], None, [3], [0, 256])
 
-			raito = hist[2] / np.sum(hist)
-			match = raito > self.threshold
-		else:
-			added = None
+            raito = hist[2] / np.sum(hist)
+            match = raito > self.threshold
+        else:
+            added = None
 
-		if debug:# and (match > threshold):
-			label = self.label if not self.label is None else self
-			print("%s(%s): result=%s raito %f orig_raito %f, threshold %3.3f orig_threshold %3.3f" %
-				(self.__class__.__name__, label, match, raito, orig_raito, self.threshold, self.orig_threshold))
-			cv2.imshow('img_fp: %s' % label, img_fp)
-			cv2.imshow('img_gray: %s' % label, img_gray)
-			cv2.imshow('mask: %s' % label, self.mask_img)
-			if not added is None:
-				cv2.imshow('result: %s' % label, added)
+        if debug:  # and (match > threshold):
+            label = self.label if not self.label is None else self
+            print("%s(%s): result=%s raito %f orig_raito %f, threshold %3.3f orig_threshold %3.3f" %
+                  (self.__class__.__name__, label, match, raito, orig_raito, self.threshold, self.orig_threshold))
+            cv2.imshow('img_fp: %s' % label, img_fp)
+            cv2.imshow('img_gray: %s' % label, img_gray)
+            cv2.imshow('mask: %s' % label, self.mask_img)
+            if not added is None:
+                cv2.imshow('result: %s' % label, added)
 
-		return match
+        return match
 
-	## Constructor.
-	# @param self                 The object.
-	# @param left                 Left of the mask.
-	# @param top                  Top of the mask.
-	# @param width                Width of the mask.
-	# @param height               Height of the mask.
-	# @param img                  Instance of the mask image.
-	# @param img_file             Filename of the mask image.
-	# @param threshold            Threshold
-	# @param orig_threshold       Target frame must be lower than this raito.
-	# @prram pre_threshold_value  Threshold target frame with this level before matching.
-	# @param debug                If true, show debug information.
-	# @param label                Label (text data) to distingish this mask.
-	def __init__(self, left, top, width, height, img = None, img_file = None, threshold = 0.9, false_positive_method = None, orig_threshold = 0.7, pre_threshold_value = 230, debug = False, label = None):
-		self.top = top
-		self.left = left
-		self.width = width
-		self.height = height
-		self.threshold = threshold
-		self.orig_threshold = orig_threshold
-		self.pre_threshold_value = pre_threshold_value
-		self.debug = debug
-		self.label = label
-		self.false_positive_method = false_positive_method if not false_positive_method is None else IkaMatcher.FP_FRONT_IS_WHITE
+    # Constructor.
+    # @param self                 The object.
+    # @param left                 Left of the mask.
+    # @param top                  Top of the mask.
+    # @param width                Width of the mask.
+    # @param height               Height of the mask.
+    # @param img                  Instance of the mask image.
+    # @param img_file             Filename of the mask image.
+    # @param threshold            Threshold
+    # @param orig_threshold       Target frame must be lower than this raito.
+    # @prram pre_threshold_value  Threshold target frame with this level before matching.
+    # @param debug                If true, show debug information.
+    # @param label                Label (text data) to distingish this mask.
+    def __init__(self, left, top, width, height, img=None, img_file=None, threshold=0.9, false_positive_method=None, orig_threshold=0.7, pre_threshold_value=230, debug=False, label=None):
+        self.top = top
+        self.left = left
+        self.width = width
+        self.height = height
+        self.threshold = threshold
+        self.orig_threshold = orig_threshold
+        self.pre_threshold_value = pre_threshold_value
+        self.debug = debug
+        self.label = label
+        self.false_positive_method = false_positive_method if not false_positive_method is None else IkaMatcher.FP_FRONT_IS_WHITE
 
-		if not img_file is None:
-			img = cv2.imread(img_file)
+        if not img_file is None:
+            img = cv2.imread(img_file)
 
-		if img is None:
-			raise Exception('Could not load mask image')
+        if img is None:
+            raise Exception('Could not load mask image')
 
-		if len(img.shape) > 2 and img.shape[2] != 1:
-			img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        if len(img.shape) > 2 and img.shape[2] != 1:
+            img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-		cropped = (img.shape[0] == self.height) and (img.shape[1] == self.width)
-		if cropped:
-			self.mask_img = img
-		else:
-			self.mask_img = img[top: top + height, left: left + width]
+        cropped = (img.shape[0] == self.height) and (
+            img.shape[1] == self.width)
+        if cropped:
+            self.mask_img = img
+        else:
+            self.mask_img = img[top: top + height, left: left + width]
