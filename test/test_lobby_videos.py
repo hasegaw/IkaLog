ccc463730952afd37d73a883bf06eea51ae00866
Takeshi HASEGAWA <hasegaw@gmail.com>
2015-10-11 01:30:44 +0900

    test/test_lobby_videos.py: Updated.
    Signed-off-by: Takeshi HASEGAWA <hasegaw@gmail.com>

--- test/test_lobby_videos.py
+++ test/test_lobby_videos.py
@@ -2,13 +2,14 @@ import unittest
 import os
 import cv2
 
+
 class TestSceneLobbyVideos(unittest.TestCase):
 
     scene_name = 'lobby'
 
     def _load_screenshot(self, filename):
         filepath = os.path.join('test_data', 'movies',
-            self.scene_name, filename)
+                                self.scene_name, filename)
 
     def noop(self, context):
         pass
@@ -39,7 +40,7 @@ class TestSceneLobbyVideos(unittest.TestCase):
         source.start_recorded_file(mp4_filename)
         source.need_resize = True
         outputPlugins = [
-            self, 
+            self,
             Screen(0, size=(640, 360)),
             DebugLog(dir='./debug/', screenshot=True),
         ]
@@ -58,11 +59,15 @@ class TestSceneLobbyVideos(unittest.TestCase):
         for event_name in list(self.event_tickets.keys()):
             if self.event_tickets[event_name] is None:
                 continue
-            assert self.event_tickets[event_name] == 0, 'Missed event %s? %s tickets remained.' % event_name
+            assert self.event_tickets[event_name] == 0, 'Missed event %s? %s tickets remained.' % (
+                event_name, self.event_tickets[event_name])
 
         assert len(self.errors) == 0, '\n'.join(self.errors)
 
-        return self.engine
+        # unreference the engine
+        engine = self.engine
+        self.engine = None
+        return engine
 
     def test_lobby_tag_2_matching_to_matched(self):
         self.event_tickets = {
@@ -75,11 +80,45 @@ class TestSceneLobbyVideos(unittest.TestCase):
             'on_frame_read': None,
         }
 
-        engine = self._test_engine('test_data/movies/lobby/lobby_tag_2_matching_to_matched.mp4')
+        engine = self._test_engine(
+            'test_data/movies/lobby/lobby_tag_2_matching_to_matched.mp4')
 
         assert engine.context['lobby']['type'] == 'tag'
         assert engine.context['lobby']['state'] == 'matched'
         assert engine.context['lobby']['team_members'] == 2
+
+    def test_lobby_tag_2_matched(self):
+        self.event_tickets = {
+            # Events expected
+            'on_lobby_matched': 1,
+
+            # Events don't care
+            'on_debug_read_next_frame': None,
+            'on_frame_read': None,
+        }
+        engine = self._test_engine(
+            'test_data/movies/lobby/lobby_tag_2_matched.mp4')
+
+        assert engine.context['lobby']['type'] == 'tag'
+        assert engine.context['lobby']['state'] == 'matched'
+        assert engine.context['lobby']['team_members'] == 2
+
+    def test_lobby_tag_4_matched(self):
+        self.event_tickets = {
+            # Events expected
+            'on_lobby_matched': 1,
+
+            # Events don't care
+            'on_debug_read_next_frame': None,
+            'on_frame_read': None,
+        }
+
+        engine = self._test_engine(
+            'test_data/movies/lobby/lobby_tag_4_matched.mp4')
+
+        assert engine.context['lobby']['type'] == 'tag'
+        assert engine.context['lobby']['state'] == 'matched'
+        assert engine.context['lobby']['team_members'] == 4
 
     def test_lobby_public_matching_to_matched(self):
         self.event_tickets = {
@@ -92,8 +131,25 @@ class TestSceneLobbyVideos(unittest.TestCase):
             'on_frame_read': None,
         }
 
-        engine = self._test_engine('test_data/movies/lobby/lobby_public_matching_to_matched.mp4')
+        engine = self._test_engine(
+            'test_data/movies/lobby/lobby_public_matching_to_matched.mp4')
 
         assert engine.context['lobby']['type'] == 'public'
         assert engine.context['lobby']['state'] == 'matched'
 
+    def test_lobby_fes_matching_to_matched(self):
+        self.event_tickets = {
+            # Events expected
+            'on_lobby_matched': 1,
+            'on_lobby_matching': None,  # フェスのマッチング中は未実装
+
+            # Events don't care
+            'on_debug_read_next_frame': None,
+            'on_frame_read': None,
+        }
+
+        engine = self._test_engine(
+            'test_data/movies/lobby/lobby_fes_matching_to_matched.mp4')
+
+        assert engine.context['lobby']['type'] == 'festa'
+        assert engine.context['lobby']['state'] == 'matched'
