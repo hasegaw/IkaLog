29549dd5f5a9d6840466505c211fa1819e1d7bf9
Takeshi HASEGAWA <hasegaw@gmail.com>
2016-04-09 22:10:53 +0900

    test: Redesigned scene unit test
    Signed-off-by: Takeshi HASEGAWA <hasegaw@gmail.com>

---
+++ test/ci/download.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+export WGET="wget -m"
+cd test/data/
+${WGET} https://dl.dropboxusercontent.com/u/14421778/IkaLog/test/scoreboard_ja_nawabari_won.png
+${WGET} https://dl.dropboxusercontent.com/u/14421778/IkaLog/test/scoreboard_ja_nawabari_lost.png
