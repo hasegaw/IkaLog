9e80a16ff8ff475570a3c8b2cc5b70ab7af15bcf
Takeshi HASEGAWA <hasegaw@gmail.com>
2016-04-27 03:01:26 +0900

    utils/config_loader: Moved from root
    Signed-off-by: Takeshi HASEGAWA <hasegaw@gmail.com>

--- IkaLog.py
+++ IkaLog.py
@@ -24,10 +24,10 @@ Localization.print_language_settings()
 import argparse
 import signal
 import time
-import IkaConfigLoader
 from ikalog import inputs
 from ikalog.engine import *
 from ikalog.utils import *
+from ikalog.utils import config_loader
 
 
 
@@ -75,7 +75,7 @@ if __name__ == "__main__":
     signal.signal(signal.SIGINT, signal_handler)
 
     args = get_args()
-    capture, OutputPlugins = IkaConfigLoader.config(args)
+    capture, OutputPlugins = config_loader.config(args)
 
     if isinstance(capture, inputs.CVFile):
         pos_msec = args.get('time_msec') or time_to_msec(args.get('time') or '0:0')
