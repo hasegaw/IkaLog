36881a2f981a4ca3b733b9e859c83bc6aa92e979
Takeshi HASEGAWA <hasegaw@gmail.com>
2016-05-05 03:37:10 +0900

    ikalog/engine: Fixed undefined reference (again)
    Signed-off-by: Takeshi HASEGAWA <hasegaw@gmail.com>

--- ikalog/engine.py
+++ ikalog/engine.py
@@ -207,7 +207,7 @@ class IkaEngine:
             # end_time should be initialized in GameFinish.
             # This is a fallback in case GameFinish was skipped.
             self.context['game']['end_time'] = IkaUtils.getTime(self.context)
-            self.context['game']['end_offset_msec'] = context['engine']['msec']
+            self.context['game']['end_offset_msec'] = self.context['engine']['msec']
 
 
         self.call_plugins('on_game_session_end')
@@ -220,7 +220,7 @@ class IkaEngine:
             # end_time should be initialized in GameFinish or session_close.
             # This is a fallback in case they were skipped.
             self.context['game']['end_time'] = IkaUtils.getTime(self.context)
-            self.context['game']['end_offset_msec'] = context['engine']['msec']
+            self.context['game']['end_offset_msec'] = self.context['engine']['msec']
 
         self.call_plugins('on_game_session_abort')
         self.reset()
