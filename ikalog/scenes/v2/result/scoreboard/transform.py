db411648fd46b7a60c5633240743558dfb20123b
Takeshi HASEGAWA <hasegaw@gmail.com>
2017-01-21 16:15:24 +0900

    scenes/v2/result/scoreboard/transform: Small.
    Signed-off-by: Takeshi HASEGAWA <hasegaw@gmail.com>

--- ikalog/scenes/v2/result/scoreboard/transform.py
+++ ikalog/scenes/v2/result/scoreboard/transform.py
@@ -20,7 +20,7 @@
 
 import cv2
 
-_center = (int(1280*0.75), int(720*0.5))
+_center = (int(1280 * 0.75), int(720 * 0.5))
 _top_win = 61
 _top_lose = 404
 _height = 300
@@ -28,15 +28,15 @@ _height = 300
 mat_win_team = cv2.getRotationMatrix2D(_center, -2.0, 1.0)
 mat_lose_team = cv2.getRotationMatrix2D(_center, 2.0, 1.0)
 
+
 def transform_scoreboard(frame):
     #assert frame.shape[0] == 720
     #assert frame.shape[1] == 1280
 
-    dim  =(frame.shape[1], frame.shape[0])
+    dim = (frame.shape[1], frame.shape[0])
 
     img_win_team = cv2.warpAffine(frame, mat_win_team, dim)
     img_lose_team = cv2.warpAffine(frame, mat_lose_team, dim)
-
 
     return {
         'win': img_win_team[_top_win:_top_win + _height, 640:, :],
@@ -44,10 +44,14 @@ def transform_scoreboard(frame):
     }
 
 if __name__ == '__main__':
-    img = cv2.imread('/Users/hasegaw/Dropbox/project_IkaLog/v2/raw_images/ja/result_socreboard.png', 1)
+    img = cv2.imread(
+        '/Users/hasegaw/Dropbox/project_IkaLog/v2/raw_images/ja/result_socreboard.png', 1)
     print(img.shape)
     r = transform_scoreboard(img)
     cv2.imshow('win', r['win'])
     cv2.imshow('lose', r['lose'])
+
+    cv2.imwrite('/tmp/win.png', r['win'])
+    cv2.imwrite('/tmp/lose.png', r['lose'])
 
     cv2.waitKey()
